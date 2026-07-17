a5def4f91fc5dd0d49c6f514f225238058a7d0ec
aleapc <47722878+aleapc@users.noreply.github.com>
2026-07-17 09:00:30 -0300

    deploy: PWA 2026-07-17T12:00Z

---
+++ _app/immutable/entry/start.DxeB27Il.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BzsYzgsU.js";import{y as t}from"../chunks/Di8OsdDM.js";export{t as load_css,r as start};
--- src/routes/+layout.js
+++
@@ -1,3 +0,0 @@
-// Estático puro: nada de servidor, tudo pré-renderizado, o cliente lê o JSON.
-export const prerender = true;
-export const ssr = false;
